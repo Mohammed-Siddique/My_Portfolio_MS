2b11d81789209a4180b9a48df4596793fcec99cf
Mohammed Siddique <khanshahid9066@gmail.com>
2025-01-29 21:19:00 +0530

    Portfolio_MERN
    DOM manipulation using javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// selecting all section content
+const sections = document.querySelectorAll('.section');
+// selecting entire controll div
+const sectBtns = document.querySelectorAll('.controlls');
+// selecting individual divs
+const secBtn = document.querySelectorAll('.control');
+// selecting body content
+const allSections = document.querySelector('.main-content');
