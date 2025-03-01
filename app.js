62d7305506da28d2583ba182f6b9559bf1b3c0cf
Mohammed Siddique <khanshahid9066@gmail.com>
2025-03-01 21:23:04 +0530

    Portfolio_MERN
    DOM manipulation using javascript

--- app.js
+++ app.js
@@ -28,3 +28,13 @@ function PageTransition(){
                 btn.classList.remove('active')
             })
             e.target.classList.add('active')
+
+            // hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+    })
